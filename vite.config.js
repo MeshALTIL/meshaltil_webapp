486f0a85e965ef021ed3b9d9b48ea484f7fe0d74
tnahrf <tkni@protonmail.com>
2025-09-26 01:24:00 +0300

    fix gh-pages config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
-	base: '/meshtastic_alt_il'
+	base: '/meshtastic_alt_il/'
 });
